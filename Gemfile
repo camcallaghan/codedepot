a974bdf65dea44ce48703be95dbd73a75dcbe805
Colum O' Callaghan <camcallaghan@gmail.com>
2015-12-26 22:50:57 +0000

    Quotes and config changes in form creation, simpleform addition

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'sprockets', '2.11.0'
 gem 'sass-rails'
 
+#Adding Simpleform gem to handle our form
+gem 'simple_form'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Splurty::Application.routes.draw do
 #Adding the below line in connection with root allows us to build out a blank controller
   root 'quotes#index'
+
+  resources :quotes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

ce90701b353cb5c61f97d506ecd413a87de15cdd
Colum O' Callaghan <camcallaghan@gmail.com>
2018-07-31 08:32:14 +0000

    Turbolinks error resolution

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require popper
-//= require tether
-//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
